107014516e2e3e2ef12dcdaa78a1bf66f164272f
Alibek Yessetov <alibek.yesetov@bk.ru>
2025-06-01 18:09:12 +0500

    migration host

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ import Knex from "knex";
 const config = {
   client: "mysql2",
   connection: {
-    host: "mariadb",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
