18121bf6214a9f9be42d8e6993a7ab75ed75b535
Alibek Yessetov <alibek.yesetov@bk.ru>
2025-06-01 19:40:54 +0500

    add accounts migration

---
+++ migrations/20250601144026_accounts.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable('accounts', (table) => {
+        table.increments("id").primary().unique();
+        table.string("username").notNullable();
+        table.string("email").notNullable().unique();
+        table.string("password").notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTableIfExists("accounts");
+};
