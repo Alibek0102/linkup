488af2a95a26c67352a31f0cecd321ebee9b7d80
Alibek Yessetov <alibek.yesetov@bk.ru>
2025-06-28 21:12:09 +0500

    add migration

---
+++ migrations/20250628160627_interests.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable('interests', (table) => {
+        table.increments("id").primary().unique();
+        table.string("name").notNullable();
+        table.string("type").notNullable();
+        table.string("icon");
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTableIfExists("interests");
+};
