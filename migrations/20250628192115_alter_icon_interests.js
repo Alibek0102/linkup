42189a41df05cb53f5c7082d4ff90a7855f23da2
Alibek Yessetov <alibek.yesetov@bk.ru>
2025-06-29 00:22:36 +0500

    complete

---
+++ migrations/20250628192115_alter_icon_interests.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.alterTable('interests', (table) => {
+        table.text('icon').alter();
+    });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.alterTable('interests', (table) => {
+        table.string('icon', 255).alter();
+    });
+}
